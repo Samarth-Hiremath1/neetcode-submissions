15523f0597d5f3bca77e7e0f3cfe524755e1f35e
Samarth Hiremath <samhiremath@ucdavis.edu>
2026-05-11 20:49:33 -0700

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.py
@@ -0,0 +1,75 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+'''
+Optimal solution: recursive DFS
+
+at each node, add 1 (current node) + max(left subtree depth, right subtree depth)
+recursive calls for each
+
+'''
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+        '''
+        OPTIMAL SOLUTION
+        # use recursive DFS
+        
+        # Base Case: if no node: return 0
+        if not root:
+            return 0
+
+        # recurssive call
+        # return 1 + max(dfs.left, dfs.right)
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right ))
+
+        '''
+
+
+        '''
+        iterative BFS
+
+        travese level by level 
+        count the # of levels we have
+
+        return levels
+        
+
+        use queue
+
+        add root node
+        pop the node and replace it with its children
+        +1 to the level number if it has children
+
+        keep repeating until all nodes get popped
+        return the level number
+
+        '''
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+
+        while q:
+            # remove every element in the que + add children (travese level + add next level)
+            for i in range(len(q)):
+                # add node's children if they aren't Null
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            level += 1
+        return level
+        
+        
+
+
+
