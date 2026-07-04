961ca74818b4756cc384238aa96af33880e02ec4
Samarth Hiremath <samhiremath@ucdavis.edu>
2026-07-04 12:10:59 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,23 @@
+'''
+buy low sell high
+
+
+
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        maxP = 0
+        
+        l = 0
+        for r in range(len(prices)):
+            if (prices[l] < prices[r]):
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            
+            if (prices[l] > prices[r]):
+                l = r
+            r += 1
+
+        return maxP
