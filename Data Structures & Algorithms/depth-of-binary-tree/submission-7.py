953839882389c674b0faf61710f4a24ecbb64ced
Samarth Hiremath <samhiremath@ucdavis.edu>
2026-07-14 08:13:49 -0700

    Add: depth-of-binary-tree - submission-7

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-7.py
@@ -0,0 +1,71 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        levels = 0
+
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            levels += 1
+        return levels
+                
+
+        
+        
+        '''
+        # recursive DFS
+        if not root:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        '''
+
+        '''
+        # iterative BFS
+        if not root:
+            return 0
+        
+        levels = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            levels += 1
+        return levels
+        '''
+
+        if not root:
+            return 0
+
+        stack = [[root, 1]]
+        res = 1
+        
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+        return res
+
+        
