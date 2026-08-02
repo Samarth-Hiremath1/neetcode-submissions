4c50af12a91c8a388ae0f33b090275e2f0d8ecde
Samarth Hiremath <samhiremath@ucdavis.edu>
2026-08-01 17:00:56 -0700

    Add: clone-graph - submission-5

---
+++ Data Structures & Algorithms/clone-graph/submission-5.py
@@ -0,0 +1,56 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+'''
+
+hashmap: oldToNew
+
+dfs:
+    if node exists in oldToNew
+        return oldToNew[node]
+    
+    # copy value
+    new = Node(node.val)
+
+    # copy neighbors
+    for neiNodes in node.neighbors:
+        new.neighbors.append(dfs(neiNodes))
+    
+    return new
+
+dfs(node)
+
+'''
+
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        
+        if not node:
+            return None
+        
+        oldToNew = {}
+
+        def dfs(node):
+            # if copy already exists
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            # copy value
+            new = Node(node.val)
+            oldToNew[node] = new
+
+            # copy neighbors
+            for nNode in node.neighbors:
+                new.neighbors.append(dfs(nNode))
+            
+            return new
+
+        return dfs(node)
+
+
