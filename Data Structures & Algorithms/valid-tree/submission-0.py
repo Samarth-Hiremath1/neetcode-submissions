caefceafc611ec1cbcf5e3eb212355cfe05ee26d
Samarth Hiremath <samhiremath@ucdavis.edu>
2026-08-02 02:55:32 -0700

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,62 @@
+'''
+edge case: empty n --> return True
+
+create adj. list
+
+iterate through each one exactly once, while tracking seen setlist
+
+def dfs(node, prev):
+    if node in seen: --> loop
+        return False
+    
+    add node to seen
+    dfs on all neighbors
+    for neighborNode of node:
+        if neighborNode == node:
+            continue
+        if dfs(neighborNode, node) == False:
+            return False
+    
+    return true
+
+if dfs(0,-1) and n==len(seen):
+    return True
+else:
+    return False
+
+
+'''
+
+from collections import defaultdict
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        adj = defaultdict(list)
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        seen = set()
+        
+        def dfs(node, prev):
+            if node in seen:
+                return False
+            
+            seen.add(node)
+            for nei in adj[node]:
+                if nei == prev:
+                    continue
+                if not dfs(nei, node):
+                    return False
+            return True
+        
+        if dfs(0, -1) and n == len(seen):
+            return True
+        else:
+            return False
+
+
+
+
