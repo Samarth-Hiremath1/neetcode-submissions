9d5a115d159632b5138a7cb050198287c6bcfc6f
Samarth Hiremath <samhiremath@ucdavis.edu>
2026-08-03 16:23:22 -0700

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} # key : value
+
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+        
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    # insert at the right most position
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+    
+    
+    
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            # update DLL positions
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        # if it overlaps, remove LRU
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
