6c4f0c04cd7b9b002998d3a4d691517dfef4b6bb
Samarth Hiremath <samhiremath@ucdavis.edu>
2026-08-03 17:14:39 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+
+        closeToOpen = {")" : "(", "}" : "{", "]" : "["}
+
+        for c in s:
+            if stack and c in closeToOpen:
+                if stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+                
+            else:
+                stack.append(c)
+        
+        if not stack:
+            return True
+        else:
+            return False
